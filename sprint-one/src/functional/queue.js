64e111e9cea101ca4028cc3ac4dd4eda295a6d2c
rdkerr <rogerdkerr@gmail.com>
2019-01-04 16:58:31 -0700

    Finished functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,31 @@
 var Queue = function() {
   var someInstance = {};
-
+  var queueSize = 0;
+  var lastKey = 0;
+  var front = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[lastKey++] = value;
+    queueSize++;
   };
 
   someInstance.dequeue = function() {
+    if (queueSize > 0){
+      var deleted = storage[front];
+      delete storage[front];
+      front++;
+      queueSize--;
+      return deleted;
+    }
+
   };
 
   someInstance.size = function() {
+    return queueSize;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,25 @@
 var Stack = function() {
   var someInstance = {};
-
+  var stackSize = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[stackSize++] = value;
   };
 
   someInstance.pop = function() {
+    if (stackSize > 0){
+      stackSize--;
+      var deleted = storage[stackSize];
+      delete storage[stackSize];
+      return deleted;
+    }
   };
 
   someInstance.size = function() {
+    return stackSize;
   };
 
   return someInstance;
