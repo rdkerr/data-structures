efb363405a263cbb0a8c9fc8ffb2e6f0b3da6468
rdkerr <rogerdkerr@gmail.com>
2019-01-04 20:37:34 -0700

    Finished prototypical

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.queueSize = 0;
+  someInstance.front = 0;
+  someInstance.back = 0;
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.back++] = value;
+    this.queueSize++;
+
+  },
+
+  dequeue: function() {
+    if (this.queueSize > 0) {
+      var deleted = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front++;
+      this.queueSize--;
+      return deleted;
+    }
+  },
+
+  size: function() {
+    return this.queueSize;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,45 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.stackSize = 0;
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.stackSize++] = value;
+  },
 
+  pop: function() {
+    if (this.stackSize > 0) {
+    this.stackSize--;
+    var deleted = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return deleted;
+    }
+  },
+
+  size: function() {
+    return this.stackSize;
+  }
+};
+
+//// var stackMethods = {};
+//Stack.prototype.push = function(value) {
+//  this.storage[this.stackSize++] = value;
+//};
+//
+//Stack.prototype.pop = function() {
+//  if (stackSize > 0) {
+//    var deleted = this.storage[--this.stackSize];
+//    delete this.storage[this.stackSize];
+//    return deleted;
+//  }
+//};
+//
+//Stack.prototype.size = function() {
+// return this.stackSize;
+//};
 
