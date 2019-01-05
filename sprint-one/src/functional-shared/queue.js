9fc09645038f01e53852223549882314e2f15501
rdkerr <rogerdkerr@gmail.com>
2019-01-04 17:42:09 -0700

    Finished functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.queueSize = 0;
+  someInstance.storage = {};
+  someInstance.front = 0;
+  someInstance.back = 0;
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
+  someInstance.size = queueMethods.size;
+  return someInstance;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value) {
+  this.storage[this.back++] = value;
+  this.queueSize++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.queueSize > 0) {
+    var deleted = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    this.queueSize--;
+    return deleted;
+  }
+};
+
+queueMethods.size = function() {
+  return this.queueSize;
+};
+
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.stackSize = 0;
+  someInstance.storage = {};
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+  return someInstance;
 };
 
 var stackMethods = {};
+stackMethods.push = function(value) {
+  this.storage[this.stackSize++] = value;
+};
 
+stackMethods.pop = function() {
+  if (this.stackSize > 0) {
+    this.stackSize--;
+    var deleted = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return deleted;
+  }
+};
 
+stackMethods.size = function() {
+  return this.stackSize;
+};
+
+var stack = Stack();
+stack.push('a');
